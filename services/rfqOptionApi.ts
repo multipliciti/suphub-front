df3492a6381a3bd0cb1117b0d9991690a4daac18
Liubomyr Tsaryniak <liutsar346@gmail.com>
2023-11-08 14:58:02 +0000

    Seller verification backend

--- services/rfqOptionApi.ts
+++ services/rfqOptionApi.ts
@@ -4,7 +4,7 @@ import { RfqOption } from '@/types/services/rfq';
 export const RfqOptionApi = (instance: AxiosInstance) => ({
 	async create(data: RfqOption) {
 		try {
-			const url = `/rfq/option`;
+			const url = `/rfq/option/own`;
 			const response = await instance.post(url, data);
 			return response;
 		} catch (error) {
