e7c909f4d1c701c786107957121bf86d24c96900
Petro <work.petrosnieda@gmail.com>
2023-12-11 23:55:11 +0200

    start refactoring

--- app/projects/(special)/order/[id]/page.tsx
+++ app/projects/(special)/order/[id]/page.tsx
@@ -1,9 +1,5 @@
 import { ProjectsBuyerOrderItem } from '@/components/Screens/Projects/ProjectsBuyerOrderItem';
 
-<<<<<<< HEAD
-export default function ProjectsOrderItem() {
-	return <ProjectsBuyerOrderItem id={3} />;
-=======
 interface Props {
 	params: {
 		id: number;
@@ -11,5 +7,4 @@ interface Props {
 }
 export default function ProjectsOrderItem({ params: { id } }: Props) {
 	return <ProjectsBuyerOrderItem id={id} />;
->>>>>>> 25fdac4f17cfa965a4e7a9cc5b430a29ef1ecace
 }
--- components/Screens/Storefront/StorefrontSellerOrderItem/Order/ProgressOrder/ProgressStepDone/index.tsx
+++ components/Screens/Storefront/StorefrontSellerOrderItem/Order/ProgressOrder/ProgressStepDone/index.tsx
@@ -1,14 +1,14 @@
 'use client';
 import Image from 'next/image';
 
-import s from './Done.module.scss';
+import s from './ProgressStepDone.module.scss';
 import success_img from '@/imgs/ResetPassword/success.svg';
 
 type TypeProps = {
 	title: string;
 };
 
-export const Done = ({ title }: TypeProps) => {
+export const ProgressStepDone = ({ title }: TypeProps) => {
 	return (
 		<div className={s.wrapper}>
 			<Image src={success_img} alt="success_img" width={150} height={150} />
