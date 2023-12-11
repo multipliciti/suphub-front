766f3b16d18327ceff73f1b02f356b534946c922
Oleksandr Stetsiuk <oleksandrstetsiuk2000@gmail.com>
2023-12-11 13:48:40 +0000

    Added a type for the root reducer, implemented a provider for receiving company data depending on the user's role

--- components/Providers/authProvider.tsx
+++ components/Providers/authProvider.tsx
@@ -1,28 +1,18 @@
 import { FC, PropsWithChildren, useEffect } from 'react';
 
 import { useAppDispatch, useAppSelector } from '@/redux/hooks';
+import { setUser, setStatusGetUser } from '@/redux/slices/auth';
 import { Api } from '@/services';
-import {
-	setUser,
-	setStatusGetUser,
-	setSellerCompany,
-	setBuyerCompany,
-} from '@/redux/slices/auth';
 
 export const AuthProvider: FC<PropsWithChildren> = ({ children }) => {
 	const api = Api();
 	const dispatch = useAppDispatch();
+
 	const statusGetUser = useAppSelector((state) => state.authSlice.statusGetUser);
 
 	const fetchUser = async () => {
 		try {
 			const { data } = await api.auth.getUser();
-
-			if (data.role === 'seller' && data.sellerCompanyId) {
-				await fetchSellerCompany(data.sellerCompanyId);
-			} else if (data.role === 'buyer' && data.buyerCompanyId) {
-				await fetchBuyerCompany(data.buyerCompanyId);
-			}
 
 			dispatch(setStatusGetUser('success'));
 			dispatch(setUser(data));
@@ -30,24 +20,6 @@ export const AuthProvider: FC<PropsWithChildren> = ({ children }) => {
 			dispatch(setStatusGetUser('rejected'));
 			dispatch(setUser(null));
 			console.error('Error fetching user data:', errorResponse);
-		}
-	};
-
-	const fetchSellerCompany = async (sellerCompanyId: number) => {
-		try {
-			const response = await api.sellerCompany.getById(sellerCompanyId);
-			dispatch(setSellerCompany(response.data));
-		} catch (e) {
-			dispatch(setSellerCompany(null));
-		}
-	};
-
-	const fetchBuyerCompany = async (buyerCompanyId: number) => {
-		try {
-			const response = await api.buyerCompany.getById(buyerCompanyId);
-			dispatch(setBuyerCompany(response.data));
-		} catch (e) {
-			dispatch(setBuyerCompany(null));
 		}
 	};
 
