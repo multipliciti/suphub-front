64289b7d896cfe0b38a8ce4eb3c3ffd69b217068
liutsar1337 <liutsar346@gmail.com>
2024-06-24 12:01:19 +0300

    fix: delete IntercomScript (replaced by library)

--- components/Providers/IntercomScript.tsx
+++ components/Providers/IntercomScript.tsx
@@ -1,5 +1,4 @@
 'use client';
-import Script from 'next/script';
 import { useAppSelector } from '@/redux/hooks';
 import { useEffect } from 'react';
 import { initializeIntercomScript } from '@/utils/initializeIntercomScript';
@@ -11,9 +10,5 @@ export const IntercomScript = () => {
 		if (user) initializeIntercomScript(user);
 	}, [user]);
 
-	return (
-		<Script id="intercom">
-			{`(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/strk46bw';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(document.readyState==='complete'){l();}else if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();`}
-		</Script>
-	);
+	return null;
 };
